c64af774a4814161327acf540e0aca64ee8fdb8f
Tim Siegler <tim.siegler@fincon.eu>
2021-03-13 18:41:11 +0100

    Add middleware and url encoder

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ const path = require('path')
 
 // Importing express
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const quotes = require('./quotes')
 
